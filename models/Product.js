ce2e69409241235b79e13e3ce657329e88432f4c
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-02-09 16:35:51 -0600

    model project.js done

--- models/Product.js
+++ models/Product.js
@@ -2,14 +2,44 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// TODO: set up fields and rules for Product model
+// TODO: set up fields and rules for Product model - DONE
 Product.init(
   {
-    // TODO: define columns
+    // TODO: define columns - DONE
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isDecimal: true
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      isNumeric: true
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Category,
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
